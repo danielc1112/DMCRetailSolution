60a3b86d4fe742881b18448503903958790d3672
danielc1112 <36406014+danielc1112@users.noreply.github.com>
2018-03-28 19:50:44 +1100

    Add files via upload

---
+++ PosWebService/IPosWebService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using DataAccess.Entity.Entities;
+
+namespace PosWebService
+{
+    [ServiceContract]
+    public interface IPosWebService
+    {
+        [OperationContract]
+        List<Product> GetProducts();
+        [OperationContract]
+        List<Customer> GetCustomers();
+        [OperationContract]
+        List<Employee> GetEmployees();
+        [OperationContract]
+        List<Supplier> GetSuppliers();
+
+        [OperationContract]
+        void UploadSale(Sale sale);
+        [OperationContract]
+        void UploadGrn(Grn grn);
+        [OperationContract]
+        void UploadStocktake(Stocktake stocktake);
+    }
+}
---
+++ PosWebService/PosWebService.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using DataAccess;
+using DataAccess.Entity.Entities;
+
+namespace PosWebService
+{
+    public class PosWebService : IPosWebService
+    {
+        public List<Product> GetProducts()
+        {
+            return DBRepository.GetProducts();
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return DBRepository.GetCustomers();
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return DBRepository.GetEmployees();
+        }
+
+        public List<Supplier> GetSuppliers()
+        {
+            return DBRepository.GetSuppliers();
+        }
+
+
+        public void UploadSale(Sale sale)
+        {
+            DBRepository.SaveSale(sale);
+        }
+
+        public void UploadGrn(Grn grn)
+        {
+            DBRepository.SaveGrn(grn);
+        }
+
+        public void UploadStocktake(Stocktake stocktake)
+        {
+            DBRepository.SaveStocktake(stocktake);
+        }
+    }
+}
