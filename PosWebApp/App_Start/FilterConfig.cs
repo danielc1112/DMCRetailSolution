b7fe3f3ffaafd892216737380df7750d94789207
danielc1112 <36406014+danielc1112@users.noreply.github.com>
2018-03-28 20:08:24 +1100

    Add files via upload

---
+++ PosWebApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PosWebApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PosWebApp/App_Start/RouteConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PosWebApp
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Sell",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Sell", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Products",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Products", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Sales",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Sales", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "SaleDetails",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "SaleDetails", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
