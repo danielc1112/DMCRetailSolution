b1643b9a513fb8a37bf06d89bae61b2e608df409
danielc1112 <36406014+danielc1112@users.noreply.github.com>
2018-03-28 19:59:11 +1100

    Add files via upload

---
+++ PosWebServiceHost/PosWebService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace PosWebServiceHost
+{
+    public partial class PosWebWindowsService : ServiceBase
+    {
+        ServiceHost host;
+        public PosWebWindowsService()
+        {
+            InitializeComponent();
+            this.AutoLog = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            host = new ServiceHost(typeof(PosWebService.PosWebService));
+            host.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (host != null)
+               host.Close();
+        }
+    }
+}
