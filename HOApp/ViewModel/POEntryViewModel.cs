8eee0915a5b04274ba8ac99afe93aafdbf4fa7d4
danielc1112 <36406014+danielc1112@users.noreply.github.com>
2018-04-17 20:49:45 +1000

    Add files via upload

--- HOApp/ViewModel/POEntryViewModel.cs
+++ HOApp/ViewModel/POEntryViewModel.cs
@@ -15,10 +15,12 @@ namespace HOApp.ViewModel
 {
     class POEntryViewModel : DataVMBase
     {
-        private RetailDbContext db = new RetailDbContext();
+        public RetailDbContext db = new RetailDbContext();
 
         public POVM PO { get; set; }
         public ObservableCollection<SupplierVM> Suppliers { get; set; }
+        public ObservableCollection<StoreVM> Stores { get; set; }
+        public ObservableCollection<ProductVM> Products { get; set; }
 
         private SupplierVM selectedSupplier;
         public SupplierVM SelectedSupplier
@@ -34,13 +36,28 @@ namespace HOApp.ViewModel
             }
         }
 
+        private StoreVM selectedStore;
+        public StoreVM SelectedStore
+        {
+            get
+            {
+                return selectedStore;
+            }
+            set
+            {
+                selectedStore = value;
+                RaisePropertyChanged();
+            }
+        }
+
         public POEntryViewModel() : base() { }
 
         protected async override void GetData(string filter = "")
         {
+            //TODO: The throbber in each screen works, but it's covered in the UI
             ThrobberVisible = Visibility.Visible;
 
-            //Supplier dropdown
+            //Supplier dropdown TODO: Should only look at active suppliers
             ObservableCollection<SupplierVM> _suppliers = new ObservableCollection<SupplierVM>();
             var suppliers = await (from s in db.Suppliers
                                    orderby s.Description
@@ -52,7 +69,31 @@ namespace HOApp.ViewModel
             Suppliers = _suppliers;
             RaisePropertyChanged("Suppliers");
 
-            //Create new PO in memory
+            //Store dropdown TODO: Should only look at active stores
+            ObservableCollection<StoreVM> _stores = new ObservableCollection<StoreVM>();
+            var stores = await (from s in db.Stores
+                                   orderby s.Description
+                                   select s).ToListAsync();
+            foreach (Store store in stores)
+            {
+                _stores.Add(new StoreVM { IsNew = false, TheEntity = store });
+            }
+            Stores = _stores;
+            RaisePropertyChanged("Stores");
+
+            //Product dropdown TODO: Should only look at products with an active status
+            ObservableCollection<ProductVM> _products = new ObservableCollection<ProductVM>();
+            var products = await (from s in db.Products
+                                orderby s.Description
+                                select s).ToListAsync();
+            foreach (Product product in products)
+            {
+                _products.Add(new ProductVM { IsNew = false, TheEntity = product });
+            }
+            Products = _products;
+            RaisePropertyChanged("Products");
+
+            //Create new PO in memory TODO: Add it in memory to db.PO.Add()?? Yes, I think so
             POVM _PO = new POVM();
             PO = _PO;
             RaisePropertyChanged("PO");
