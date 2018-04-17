a8acb09822ead5bfe75b527162b6aa7bf5752fe8
danielc1112 <36406014+danielc1112@users.noreply.github.com>
2018-04-17 20:48:25 +1000

    Add files via upload

---
+++ HOApp/Model/StoreProductTranVM.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Entity.Entities;
+
+namespace HOApp.Model
+{
+    public class StoreProductTranVM : VMBase
+    {
+        public StoreProductTran TheEntity
+        {
+            get
+            {
+                return (StoreProductTran)base.theEntity;
+            }
+            set
+            {
+                theEntity = value;
+                RaisePropertyChanged();
+            }
+        }
+        public StoreProductTranVM()
+        {
+            // Initialise the entity or inserts will fail
+            TheEntity = new StoreProductTran();
+        }
+
+    }
+}
